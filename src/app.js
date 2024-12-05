12290891b4e9a23acab64b0974a14d32bdd7a516
David López <lgdavid0194@gmail.com>
2024-12-05 11:32:08 +0100

    lti authentication done

--- src/app.js
+++ src/app.js
@@ -1,86 +1,25 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import pool from './db.js';
 import AIRoutes from './routes/AIRoutes.js';
+import ltiRoutes from './routes/ltiRoutes.js';
 import databaseRoutes from './routes/databaseRoutes.js';
-import jwt from 'jsonwebtoken';
-import fs from 'fs';
-import crypto from 'crypto'
-dotenv.config();
 
+
+dotenv.config();
 const app = express();
 const port = 3001; 
-
-const privateKey = fs.readFileSync('private.pem', 'utf8'); 
-const publicKey = fs.readFileSync('public.pem', 'utf8'); 
-
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.post('/authorize', (req, res) => {
-  console.log(req.body); 
-  const clientId = req.body.client_id 
-  const loginHint = req.body.login_hint
-  const authorizationCode = crypto.randomBytes(32).toString('hex');
-  const redirectUri = req.body.redirect_uri;
-  const state = req.body.state;
-  console.log(req.body.redirect_uri);
-  
-  console.log("redirecting...");
-  res.redirect('/launch');
-});
-app.post('/launch', (req, res) => {
-    console.log("POST launch endpoint");
-    
-    const idToken = req.body.id_token; 
-  
-    if (!idToken) {
-      console.error('id_token no encontrado en la solicitud');
-      return res.status(400).send('id_token no encontrado');
-    }
-  
-    try {
-      const decoded = jwt.verify(idToken, publicKey, { algorithms: ['RS256'] });
-  
-      if (decoded.iss !== 'http://localhost/moodle') { 
-        throw new Error('Emisor no valido');
-      }
-  
-      const userId = decoded.sub;
-      const courseName = decoded['https://purl.imsglobal.org/spec/lti/claim/context'].label; 
-  
-      res.send('LTI exito'); 
-  
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(401).send('Error en la validación del JWT');
-    }
-});
-app.get('/launch', (req, res) => { 
-  res.json([
-    {
-      "client_id": "SRiEZ3Drnhs2j7Y", 
-      "login_hint": "2", 
-      "redirect_uri": "https://e2b2-81-184-197-163.ngrok-free.app/launch",
-      "nonce": "a1b2c3d4e5f6", 
-      "state": "xyz789" 
-    }
-  ])
 
-})
-  app.get('/jwks', (req, res) => { 
-  console.log("get jwks");
-  res.json({ 
-    keys: [{
-      alg: 'RS256',
-      kty: 'RSA',
-      use: 'sig',
-      x5c: [publicKey] 
-    }]
-  });
-});
 
 app.use('/api/AIGeneration', AIRoutes);
 app.use('/api/saveToDatabase', databaseRoutes);
+app.use('/lti', ltiRoutes);
 
 const startServer = async () => {
   try {
---
+++ src/routes/ltiRoutes.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import crypto from 'crypto'
+import querystring from 'querystring';
+import axios from 'axios';
+import jwkToPem from 'jwk-to-pem';
+dotenv.config();
+
+const router = express.Router();
+//URL TEMPORAL DEL NGROK, LAS DEL MOODLE SON IGUAL 
+//Configuracion URL del Moodle: https://drive.google.com/file/d/1MmKNij-B93nGPIUd7Ci_AqVAviPUVtJG/view
+router.post('/launch', async (req, res) => {
+    const target_link_uri = 'https://42c3-81-184-197-163.ngrok-free.app/lti/login'; 
+    const login_hint = req.body.login_hint
+    const lti_message_hint = req.body.lti_message_hint
+    const client_id = req.body.client_id
+    const authEndpoint = 'http://localhost/moodle/mod/lti/auth.php';
+    const state = crypto.randomBytes(8).toString('hex');
+    const nonce = crypto.randomBytes(16).toString('hex');
+    const params = {
+      client_id,
+      response_type: 'id_token',
+      scope: 'openid',
+      redirect_uri: target_link_uri,
+      login_hint,
+      lti_message_hint,
+      state: state,
+      nonce: nonce,
+      response_mode: 'form_post'
+    }
+    const redirectUrl = `${authEndpoint}?${querystring.stringify(params)}`;
+    res.redirect(redirectUrl);
+  });
+  
+router.post('/login', async (req, res) => {
+    const { id_token } = req.body;
+    if (!id_token) {
+        return res.status(400).send('ID Token no recibido.');
+    }
+    try {
+        const jwksUrl = 'http://localhost/moodle/mod/lti/certs.php';
+        const { data } = await axios.get(jwksUrl);
+
+        const { header } = jwt.decode(id_token, { complete: true });
+
+        // Encontrar la clave pública correcta
+        const publicKey = data.keys.find(key => key.kid === header.kid);
+        if (!publicKey) {
+            throw new Error('Clave pública no encontrada.');
+        }
+
+        // Convertir la clave pública en formato PEM
+        const pem = jwkToPem(publicKey);
+
+        // Verificar el JWT
+        const decoded = jwt.verify(id_token, pem, {
+            algorithms: ['RS256'],
+            audience: 'SRiEZ3Drnhs2j7Y',
+            issuer: 'http://localhost/moodle',
+        });
+
+        console.log('Token válido:', decoded);
+        res.send('Lanzamiento exitoso.');
+    } catch (err) {
+        console.error('Error al validar el token:', err.message);
+        res.status(400).send('Token inválido.');
+    }
+});
+
+export default router;
