46ec7d89136fe0f854fd8577829fe7ccff1038e9
David López <57214590+PantherKnigh@users.noreply.github.com>
2024-12-23 22:21:45 +0100

    Plugin local del moodle para importar preguntas

---
+++ qbank_plugin/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_qbank_plugin';  
+$plugin->version = 2024011500;           
+$plugin->requires = 2022041900;          
+$plugin->maturity = MATURITY_STABLE;     
+$plugin->release = '1.0.0';                
